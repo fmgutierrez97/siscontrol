f1a1e3f6b842e1d129dbc49e2c4563024e7d754b
andreybejarano <andreybejarano@gmail.com>
2017-09-18 00:06:34 -0300

    vista menu lateral

--- Vista/plantillas/_menuLateralAdmin.php
+++ Vista/plantillas/_menuLateralAdmin.php
@@ -1,64 +1,59 @@
 <div id="menuLateralAdmin" class="sidebar-collapse">
-                <ul class="nav" id="main-menu">
-				<li class="text-center">
-                    <img src="img/find_user.png" class="user-image img-responsive"/>
-					</li>
-				
-					
-                    <li>
-                        <a class=""  href="index.html"><i class="fa fa-dashboard fa-3x"></i> Ingreso de personal </a>
-                    </li>
-                     <li>
-                        <a  href=""><i class="fa fa-desktop fa-3x"></i></a>
-                    </li>
-                    <li>
-                        <a  href="1"><i class="fa fa-qrcode fa-3x"> Verificar personal </i></a>
-                    </li>
-				    <li  >
-                        <a   href=""><i class="fa fa-bar-chart-o fa-3x"></i></a>
-                    </li>	
-                      <li  >
-                        <a  href=""><i class="fa fa-table fa-3x"></i></a>
-                    </li>
-                    <li  >
-                        <a  href=""><i class="fa fa-edit fa-3x"></i> Registrar empleados </a>
-                    </li>				
-					 <li  >
-                        <a   href=""><i class="fa fa-bolt fa-3x"></i> </a>
-                    </li>	
-                     <li  >
-                        <a   href=""><i class="fa fa-laptop fa-3x"></i>Registro de usuarios</a>
-                    </li>	
-					                   
-                    <li>
-                        <a href="#"><i class="fa fa-sitemap fa-3x"></i> Multi-Level Dropdown<span class="fa arrow"></span></a>
-                        <ul class="nav nav-second-level">
-                            <li>
-                                <a href="#">Second Level Link</a>
-                            </li>
-                            <li>
-                                <a href="#">Second Level Link</a>
-                            </li>
-                            <li>
-                                <a href="#">Second Level Link<span class="fa arrow"></span></a>
-                                <ul class="nav nav-third-level">
-                                    <li>
-                                        <a href="#">Third Level Link</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Third Level Link</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Third Level Link</a>
-                                    </li>
-
-                                </ul>
-                               
-                            </li>
-                        </ul>
-                    </li>  
-                    <li >
-                        <a  href="blank.html"><i class="fa fa-square-o fa-3x"></i> Blank Page</a>
-                    </li>	
-                </ul>               
-            </div>
+    <ul class="nav" id="main-menu">
+        <li class="text-center">
+            <img src="img/find_user.png" class="user-image img-responsive"/>
+        </li>
+        <li>
+            <a class=""  href="index.html"><i class="fa fa-dashboard fa-3x"></i> Ingreso de personal </a>
+        </li>
+        <li>
+            <a  href=""><i class="fa fa-desktop fa-3x"></i></a>
+        </li>
+        <li>
+            <a  href="1"><i class="fa fa-qrcode fa-3x"> Verificar personal </i></a>
+        </li>
+        <li>
+            <a href=""><i class="fa fa-bar-chart-o fa-3x"></i></a>
+        </li>
+        <li>
+            <a href=""><i class="fa fa-table fa-3x"></i></a>
+        </li>
+        <li>
+            <a href=""><i class="fa fa-edit fa-3x"></i> Registrar empleados </a>
+        </li>		
+        <li>
+            <a href=""><i class="fa fa-bolt fa-3x"></i> </a>
+        </li>
+        <li>
+            <a href=""><i class="fa fa-laptop fa-3x"></i>Registro de usuarios</a>
+        </li>
+        <li>
+            <a href="#"><i class="fa fa-sitemap fa-3x"></i> Multi-Level Dropdown<span class="fa arrow"></span></a>
+            <ul class="nav nav-second-level">
+                <li>
+                    <a href="#">Second Level Link</a>
+                </li>
+                <li>
+                    <a href="#">Second Level Link</a>
+                </li>
+                <li>
+                    <a href="#">Second Level Link<span class="fa arrow"></span></a>
+                    <ul class="nav nav-third-level">
+                        <li>
+                            <a href="#">Third Level Link</a>
+                        </li>
+                        <li>
+                            <a href="#">Third Level Link</a>
+                        </li>
+                        <li>
+                            <a href="#">Third Level Link</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="blank.html"><i class="fa fa-square-o fa-3x"></i> Blank Page</a>
+        </li>
+    </ul>
+</div>
--- Vista/principal.php
+++ Vista/principal.php
@@ -2,21 +2,21 @@
 Vista::mostrar('plantillas/_encabezado', $datos);
 ?>
 <div class="row">
-    <div class="col-md-12 col-sm-12 col-xs-12">
-                   
+    <div class="col-md-3">
+    <?php
+    Vista::mostrar('plantillas/_menuLateralAdmin');
+    ?>    
+    </div>
+    <div class="col-md-9 col-sm-12 col-xs-12">
             <div class="panel panel-default">
-                
                 <div class="panel-heading">
-    
                 </div>
-                
             <div class="form-group input-group">
                 <input type="text" class="form-control" placeholder="Buscar empleado">
                 <span class="input-group-btn">
                     <button class="btn btn-default" type="button"><i class="fa fa-search"></i></button>
                 </span>
             </div>
-            
             <div class="panel-body">
                 <div class="table-responsive">
                     <table class="table table-striped table-bordered table-hover">
